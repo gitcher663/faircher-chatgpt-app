790015933ff9b2911b4c74d2bff04fbc94811138
gitcher663 <nick@faircher.com>
2026-01-20 20:16:05 -0500

    Update errors.ts

--- server/src/errors.ts
+++ server/src/errors.ts
@@ -1,13 +1,3 @@
-export class UpstreamError extends Error {
-  readonly status?: number;
-
-  constructor(message: string, status?: number) {
-    super(message);
-    this.name = "UpstreamError";
-    this.status = status;
-  }
-}
-
 export class ValidationError extends Error {
   constructor(message: string) {
     super(message);
